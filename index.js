de03a5a47ad3e211a65468b4d4ddb3b09cd1c200
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-18 15:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) 
     return("I can\'t hear you!")
-}
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string) 
-    return("YES INDEED!")
-}
+} else if {
+  function sayHiToGrandma(string) {
+    if (string.toUpperCase() === string) 
+      return("YES INDEED!")
+} else if
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Granma.') 
